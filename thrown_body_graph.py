d0c10ea1bc985d7642220b09714ce75fc3447771
arturtsaturyan07 <arturtsaturyan07@gmail.com>
2024-03-23 12:09:59 +0400

    modified

--- thrown_body_graph.py
+++ thrown_body_graph.py
@@ -14,7 +14,7 @@ xmax =  2 * np.cos(np.radians(a)) * np.sin(np.radians(a)) * v*v/g
 ymax = (v * np.sin(np.radians(a)))**(2)/(2 * g)
 x = np.linspace(0, xmax , 100) 
 y = x * np.tan(np.radians(a)) - (g * x**(2))/(2*(np.cos(np.radians(a))*v)**(2))
-print("S(max) = ",xmax," H(max) =",ymax)
+print("S(max) = ",''.join(list(str(xmax)))," H(max) =",''.join(list(str(ymax))))
 ax.set_title('Ֆունկցիայի գրաֆիկ')
 ax.set_xlabel('s')
 ax.set_ylabel('h')
